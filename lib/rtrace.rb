3866087c4a045be48c8f3106c70b957eacadccca
Christoph <christoph@zerodeviation.net>
2012-07-05 20:13:48 -0600

    Tweak stuff.

--- lib/rtrace.rb
+++ lib/rtrace.rb
@@ -1,5 +1,92 @@
 require "rtrace/version"
+require 'socket'
+require 'rubygems'
+require 'geocoder'
+require 'timeout'
+
+
+byebye = proc {
+  puts "\x8\x8"+"Canceled. "
+  exit(0)
+}
+
+trap('INT', byebye)
 
 module Rtrace
-  # Your code goes here...
+
+
+  class << self
+  end
+
+  def self.trace(options)
+    @options = options
+    port = 33434
+
+    @send_socket = Socket.new(:INET, :DGRAM)
+    @dest_addr = Socket.sockaddr_in(port, options[:host])
+    @dest_ip = IPSocket::getaddress(options[:host])
+    puts "Tracing #{options[:host]} ( #{@dest_ip} ) ..."
+    puts
+
+    @recv_socket = Socket.new( Socket::PF_INET, Socket::SOCK_RAW, Socket::IPPROTO_ICMP)
+    @recv_addr = Socket.sockaddr_in(port, "")
+    @recv_socket.bind(@recv_addr)
+    self.run_loop
+  end
+
+  def self.run_loop
+    ttl = @options[:first_ttl]
+    while true do
+      break if ttl>@options[:max_ttl]
+      @send_socket.setsockopt(Socket::IPPROTO_IP, Socket::IP_TTL, [ttl].pack('i'))
+      t1 = Time.now
+      @send_socket.send("", 0, @dest_addr)
+
+
+
+
+      data = ""
+      begin
+        timeout(@options[:timeout]) do
+          data = @recv_socket.recvfrom( 512 )[1]
+        end
+      rescue Timeout::Error
+        puts ttl.to_s.ljust(5)+"*"
+        ttl += 1
+        next
+      end
+
+
+
+
+      time = ((Time.now - t1)*1000).round 1
+      time = time.to_s+" ms"
+      ip = data.ip_address
+      loc = Geocoder.search(ip).first
+
+      if loc.city.empty?
+        place = ""
+      else
+        place = "#{loc.city}, #{loc.state}"
+      end
+
+      begin
+        name = Socket.getaddrinfo(ip, 0, Socket::AF_UNSPEC, Socket::SOCK_STREAM, nil, Socket::AI_CANONNAME, true)[0][2]
+      rescue
+        name = ""
+      end
+
+      s_ttl = ttl.to_s.ljust(5)
+      name = name.ljust(45)
+      s_ip = ip.ljust(20)
+      time = time.ljust(15)
+      place = place.ljust(25)
+
+      puts s_ttl+name+s_ip+time+place
+
+      ttl += 1
+      break if @dest_ip==ip
+    end
+    
+  end
 end
--- rtrace.gemspec
+++ rtrace.gemspec
@@ -2,11 +2,12 @@
 require File.expand_path('../lib/rtrace/version', __FILE__)
 
 Gem::Specification.new do |gem|
-  gem.authors       = ["Christoph"]
+  gem.authors       = ["Christoph Koehler"]
   gem.email         = ["christoph@zerodeviation.net"]
-  gem.description   = %q{traceroute in Ruby}
-  gem.summary       = %q{traceroute in Ruby with flair.}
-  gem.homepage      = ""
+  gem.description   = %q{Subset of traceroute functionality with added geocoded
+  location based on IP. Uses the geocoder gem.}
+  gem.summary       = %q{traceroute in Ruby with geocoding.}
+  gem.homepage = %q{http://rubygems.org/gems/rtrace}
 
   gem.files         = `git ls-files`.split($\)
   gem.executables   = gem.files.grep(%r{^bin/}).map{ |f| File.basename(f) }
